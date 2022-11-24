a3997e0ba126ca5a700f20683816c8fd60c5b212
karllucas <karlkamakia@gmail.com>
2022-11-24 14:00:48 +0300

    main commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import LayoutOne from "../../layouts/LayoutOne";
+import HeroSliderOne from "../../wrappers/hero-slider/HeroSliderOne";
+import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
+import TabProduct from "../../wrappers/product/TabProduct";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <MetaTags>
+        <title>Funky Phones</title>
+        <meta
+          name="description"
+          content="And there was nothing left but flipping phones"
+        />
+      </MetaTags>
+      <LayoutOne
+        headerContainerClass="container-fluid"
+        headerPaddingClass="header-padding-1"
+      >
+        {/* hero slider */}
+        <HeroSliderOne />
+
+        {/* featured icon */}
+        <FeatureIcon spaceTopClass="pt-100" spaceBottomClass="pb-60" />
+
+        {/* tab product */}
+        <TabProduct spaceBottomClass="pb-60" category="fashion" />
+      </LayoutOne>
+    </Fragment>
+  );
+};
+
+export default Home;
